6a0c90cbac4c665ed6d6717d5e099aedfba24bb5
pauldechorgnat <pdechorgnat@yahoo.fr>
2022-05-05 13:17:52 +0200

    Premier commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+api = FastAPI(title="Mon API pour les DEC21 CDE")
+
+# method: GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS, ...
+# GET /users
+# GET /users/{user_id}
+# POST /users
+# PUT /users/{user_id}
+# DELETE /users/{user_id}
+
+# url : http(s):// + mon_domaine.com + /mon/point/de/terminaison
+
+
+@api.get("/")
+def get_index():
+    return {
+        "hello": "world"
+    }
+
+
+@api.delete("/mon_delete")
+def ma_fonction():
+    """Cette fonction ne fait rien du tout. C'est juste un exemple
+    """
+    return {
+        "Ceci est": "un delete un peu différent"
+    }
+
+
+@api.post("/mon_post")
+def post_mon_post():
+    return {
+        "mon_nombre": 123
+    }
+
+
+# VSCode, Pycharms CE, Spyder
---
+++ tests.py
@@ -0,0 +1,28 @@
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+
+
+def test_get_index():
+    response = requests.get(
+        url=f"{API_URL}/"
+    )
+
+    assert response.status_code == 200, response.content
+    data = response.json()
+
+    assert data["hello"] == "world"
+
+
+def test_delete_mon_delete():
+    response = requests.delete(
+        url=f"{API_URL}/mon_delete"
+    )
+    assert response.status_code == 200, response.content
+
+
+def test_post_mon_post():
+    response = requests.post(
+        url=f"{API_URL}/mon_post"
+    )
+    assert response.status_code == 200, response.content
